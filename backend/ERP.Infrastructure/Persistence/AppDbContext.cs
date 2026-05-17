605defe11df0d053c948a7419350cc89ef7edf0e
vivekThoke <vivekthoke5@gmail.com>
2026-05-17 13:04:51 +0530

    configured the appDbContext for the pgSql

---
+++ backend/ERP.Infrastructure/Persistence/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace ERP.Infrastructure.Persistence
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+    }
+}
