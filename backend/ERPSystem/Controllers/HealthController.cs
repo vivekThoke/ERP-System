c0bf7518954814ec35c3924342c72430080fdd73
vivekThoke <vivekthoke5@gmail.com>
2026-05-17 13:30:16 +0530

    endpoint to get API health check running

---
+++ backend/ERPSystem/Controllers/HealthController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ERP.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("ERP API is running");
+        }
+    }
+}
