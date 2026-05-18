2a77d930582e77cf7d9dd9ebce78cbcc5febb6de
vivekThoke <vivekthoke5@gmail.com>
2026-05-18 09:15:55 +0530

    User Entity model with properities

---
+++ backend/ERP.Domain/Entities/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ERP.Domain.Common;
+
+namespace ERP.Domain.Entities
+{
+    public class User : BaseEntity
+    {
+        public string ?Email { get; set; }
+        public string ?PasswordHash { get; set; }
+        public string ?FullName { get; set; }
+        public string ?Role { get; set; } // Admin, Manager, Employee
+    }
+}
