0751d74275f337300768fffac601344a572be531
vivekThoke <vivekthoke5@gmail.com>
2026-05-19 22:32:25 +0530

    Implemented IUserRepsoitory for addUser and get User

---
+++ backend/ERP.Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ERP.Application.Interfaces;
+using ERP.Domain.Entities;
+using ERP.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace ERP.Infrastructure.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly AppDbContext _dbContext;
+
+        public UserRepository(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<User> GetByEmailAsync(string email)
+        {
+            return await _dbContext.Users.FirstOrDefaultAsync(x => x.Email == email);
+        }
+
+        public async Task AddAsync(User user)
+        {
+            await _dbContext.Users.AddAsync(user);
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
