0a6e68eafa35de535b4ec27c34295887e84bef07
vivekThoke <vivekthoke5@gmail.com>
2026-05-19 22:19:00 +0530

    interfaces for the IUserRepository

---
+++ backend/ERP.Application/Interfaces/IUserRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ERP.Domain.Entities;
+
+namespace ERP.Application.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task<User> GetByEmailByAsync(string email);
+        Task AddAsync(User user);
+    }
+}
