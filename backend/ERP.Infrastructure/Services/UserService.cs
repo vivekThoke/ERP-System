69f49a9d52590a51619329cbcf0e17df14255b3f
vivekThoke <vivekthoke5@gmail.com>
2026-05-20 23:09:10 +0530

    Implemented RegisterUser to add new user

--- backend/ERP.Infrastructure/Services/UserService.cs
+++ backend/ERP.Infrastructure/Services/UserService.cs
@@ -3,10 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ERP.Application.Interfaces;
+using ERP.Domain.Entities;
 
 namespace ERP.Infrastructure.Services
 {
-    internal class UserService
+    public class UserService : IUserService
     {
+        private readonly IUserRepository _userRepository;
+
+        public UserService(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task RegisterUser(string email, string password, string fullName)
+        {
+            var user = new User
+            {
+                Email = email,
+                PasswordHash = password,
+                FullName = fullName,
+                Role = "Employee"
+            };
+
+            await _userRepository.AddAsync(user);
+        }
     }
 }
