f42c2b9e9e41dd0c3fc8955729c0255b9c5c6fa4
vivekThoke <vivekthoke5@gmail.com>
2026-05-21 09:09:04 +0530

    Controller for register user with route

---
+++ backend/ERPSystem/Controllers/UserController.cs
@@ -0,0 +1,24 @@
+﻿using ERP.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ERP.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(string email, string password, string fullName)
+        {
+            await _userService.RegisterUser(email, password, fullName);
+            return Ok("User Created");
+        }
+    }
+}
